b62611a22c25da38dcf7d6a66617878653c6cf08
TomPO <tom@teapot-media.co.uk>
2024-03-24 16:55:50 +0000

    Laptop

--- CppExplore/Source/GenericProgramming/RefAndCref/RefAndCRef.cpp
+++ CppExplore/Source/GenericProgramming/RefAndCref/RefAndCRef.cpp
@@ -1,13 +1,31 @@
+// The difference between std::cref and std::ref is that cref is for const 
+
+
 #include <functional>
+#include <string>
+#include <iostream>
+
+void Print(const std::string& str)
+{
+	std::cout << str << std::endl;
+}
 
 template<typename T>
-void Fn(T x)
+void PrintT(T x)
 {
+	// x is a reference_wrapper instance
+	// 
+	// This calls the overloaded () operator in reference_wrapper
+	// Which returns the std::string* managed by the reference_wrapper by reference
+	// Which is then passed to Print
+	Print(x);
 }
 
 int main()
 {
-	int i = 5;
-	Fn(std::ref(i));
+	std::string str{ "hello" };
+
+	PrintT(std::cref(str));
+
 	return 0;
 }
